0ae97e24760fb6e98186cac8f09e051ddc9c4b0c
Ryo Nakamine <rnakamine8080@gmail.ocm>
2018-05-14 06:23:50 +0900

    correction hello_app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "¡hola, mundo!"
-  end
-
-  def goodby
-    render html: "goodby, world!"
+    render html: "hello world!"
   end
 end
